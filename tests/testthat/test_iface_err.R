9e3609363fa785862ca5966240cc16aae4ccf342
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-01-31 11:44:53 +0100

    start with error tests

---
+++ tests/testthat/test_iface_err.R
@@ -0,0 +1,21 @@
+library(nestedRanksTest)
+data(woodpecker_multiyear)
+
+context("Errors when using each interface")
+
+adat = subset(woodpecker_multiyear, Species == "agrifolia")
+
+test_that("Errors produced when groups missing", {
+    expect_error(nestedRanksTest(Distance ~ Year, data = adat),
+                "invalid group specification in formula")
+    expect_error(nestedRanksTest(Distance ~ Year | G, data = adat),
+                "object 'G' not found")
+    expect_error(nestedRanksTest(adat$Year, adat$Distance),
+                "argument \"groups\" is missing, with no default")
+})
+
+test_that("Errors produced when other variable missing", {
+    expect_error(nestedRanksTest(Year, Distance, data = adat),
+                "object 'Year' not found")
+})
+
